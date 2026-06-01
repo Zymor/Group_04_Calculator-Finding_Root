8c08dd5e9aa46a4c8d439e3a5af1b29153323962
Starling <nakboyloy50@gmail.com>
2026-06-01 23:26:41 +0700

    update

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,7 +25,19 @@ public class Main {
         
         }
 
+@Override
+    public void start(Stage stage) throws Exception {
 
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(getClass().getResource("View.fxml"));
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+        
+        stage.show();
+}
     }
     
 
